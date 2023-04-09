6ae27a93b7a74a3db18719a7375bcc299c1b46e0
frnkpsycho <frnkpsycho@gmail.com>
2023-04-09 20:50:06 +0800

    initialized environment

---
+++ src/main/java/net/frnks/reneweverything/RenewEverythingModClient.java
@@ -0,0 +1,10 @@
+package net.frnks.reneweverything;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RenewEverythingModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
--- src/main/java/net/frnks/reneweverything/mixin/ModMixin.java
+++ src/main/java/net/frnks/reneweverything/mixin/ModMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.frnks.reneweverything.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.frnks.reneweverything.RenewEverythingMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class ModMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		RenewEverythingMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
